b4c3638788958728cb4d057c715781eedafb5447
benja <benjaespinosa98@gmail.com>
2023-08-06 23:44:39 -0600

    Fix: Imports Deleted

--- app/src/main/java/com/example/practicemvvm/ui/fragments/ProductDetailFragment.kt
+++ app/src/main/java/com/example/practicemvvm/ui/fragments/ProductDetailFragment.kt
@@ -1,11 +1,8 @@
 package com.example.practicemvvm.ui.fragments
 
-import android.app.Activity
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
-import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
@@ -13,14 +10,6 @@ import com.example.practicemvvm.R
 import com.example.practicemvvm.databinding.FragmentProductDetailBinding
 import com.example.practicemvvm.ui.AdsBanner
 import com.example.practicemvvm.ui.AdsInterstitial
-import com.google.android.gms.ads.AdError
-import com.google.android.gms.ads.AdListener
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.FullScreenContentCallback
-import com.google.android.gms.ads.LoadAdError
-import com.google.android.gms.ads.MobileAds
-import com.google.android.gms.ads.interstitial.InterstitialAd
-import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 
 class ProductDetailFragment : Fragment(R.layout.fragment_product_detail) {
